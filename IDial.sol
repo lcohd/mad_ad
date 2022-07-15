9e5d4275ba3e9a9171faee636e548008fd26627c
lcohd <106416834+lcohd@users.noreply.github.com>
2022-07-15 19:31:50 +0800

    Delete IDial.sol

--- IDial.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IDial {
-    function dial(address token,address recipient, uint256 amount) external;
-    function queryToken(address token) external view returns(uint256);
-}
